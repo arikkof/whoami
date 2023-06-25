7d557ecdd6f0c36ee17e69258d9eebcba3b8e996
Alex Messner :) <113343712+c2210475026@users.noreply.github.com>
2023-06-25 18:16:42 +0200

    Revert "Some light refactoring"

--- server/history-model.js
+++ server/history-model.js
@@ -1,36 +1,35 @@
 let historyData = {};
-
 function storeData(name, data) {
-    if (!historyData[name]) {
-        historyData[name] = [];
-    }
+  if (!historyData[name]) {
+    historyData[name] = [];
+  }
 
-    // Check if data with the same name already exists
-    const existingDataIndex = historyData[name].findIndex(item => item.id === data.id);
-
-    if (existingDataIndex !== -1) {
-        // Update existing data
-        historyData[name][existingDataIndex] = data;
-    } else {
-        // Add new data
-        historyData[name].push(data);
-    }
+  // Check if data with the same name already exists
+  const existingDataIndex = historyData[name].findIndex(item => item.id === data.id);
+  
+  if (existingDataIndex !== -1) {
+    // Update existing data
+    historyData[name][existingDataIndex] = data;
+  } else {
+    // Add new data
+    historyData[name].push(data);
+  }
 }
 
 function getData(name) {
-    return historyData[name] || [];
+  return historyData[name] || [];
 }
 
 function getAllData() {
     return historyData;
-}
-
-function deleteData(name) {
+  }
+  
+  function deleteData(name) {
     if (historyData.hasOwnProperty(name)) {
-        delete historyData[name];
-        return true;
+      delete historyData[name];
+      return true;
     }
     return false;
-}
-
-module.exports = {storeData, getData, getAllData, deleteData};
+  }
+  
+  module.exports = { storeData, getData, getAllData, deleteData };  

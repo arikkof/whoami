efe125c9ab4059d50544f92a5a7b2df8156aea78
Nour-hash <113339425+Nour-hash@users.noreply.github.com>
2023-06-27 13:15:52 +0200

    some styling

--- server/client/index_homepage.js
+++ server/client/index_homepage.js
@@ -5,12 +5,12 @@ const ACCEPT_TYPE = ACCEPT_TYPE_JSON;
 
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft = "0";
+
 }
 
 let resultData=[];
